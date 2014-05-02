b44bddc6608450d0c27dc8c61ca69a34da3cb9ed
Mohammad Hasani Eghtedar <m.h.eghtedar@gmail.com>
2014-05-02 16:45:27 +0430

    Translation Module multi language

--- Packages/mod_tanzil_translation/helper.php
+++ Packages/mod_tanzil_translation/helper.php
@@ -3,7 +3,7 @@
  * @package     Tanzil.Site
  * @subpackage  mod_tanzil_translation
  *
- * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2013 - 2014 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
@@ -15,10 +15,10 @@ defined('_JEXEC') or die;
  * @package     Tanzil.Site
  * @subpackage  mod_tanzil_translation
  */
-class ModTanzilTranslationHelper
+abstract class ModTanzilTranslationHelper
 {
-	public static function getList(&$params)
+	public static function getTranslations(&$params)
 	{
-		return '';
+		return array();
 	}
 }
