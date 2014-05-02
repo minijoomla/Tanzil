2b739bf79391090916d168fa9c40f7804aeba4b6
Mohammad Hasani Eghtedar <m.h.eghtedar@gmail.com>
2014-05-02 19:45:32 +0430

    Module Search Multi language

--- Packages/mod_tanzil_search/helper.php
+++ Packages/mod_tanzil_search/helper.php
@@ -3,7 +3,7 @@
  * @package     Tanzil.Site
  * @subpackage  mod_tanzil_search
  *
- * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2013 - 2014 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
@@ -15,10 +15,10 @@ defined('_JEXEC') or die;
  * @package     Tanzil.Site
  * @subpackage  mod_tanzil_search
  */
-class ModTanzilSearchHelper
+abstract class ModTanzilSearchHelper
 {
 	public static function getList(&$params)
 	{
-		return '';
+		return array();
 	}
 }
--- Packages/mod_tanzil_search/mod_tanzil_search.php
+++ Packages/mod_tanzil_search/mod_tanzil_search.php
@@ -3,7 +3,7 @@
  * @package     Tanzil.Site
  * @subpackage  mod_tanzil_search
  *
- * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2013 - 2014 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
