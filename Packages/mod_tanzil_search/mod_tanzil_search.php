1486df0e53736f6be758cc1143d706d7883ddb56
Mohammad Hasani Eghtedar <m.h.eghtedar@gmail.com>
2014-06-06 12:56:19 +0430

    Add Joomla search to tanzil search

--- Packages/mod_tanzil_search/mod_tanzil_search.php
+++ Packages/mod_tanzil_search/mod_tanzil_search.php
@@ -12,8 +12,31 @@ defined('_JEXEC') or die;
 // Include the Tanzil functions only once
 require_once __DIR__ . '/helper.php';
 
-$list = ModTanzilSearchHelper::getList($params);
+$lang = JFactory::getLanguage();
+$app  = JFactory::getApplication();
+
+if ($params->get('opensearch', 1))
+{
+	$doc = JFactory::getDocument();
+
+	$ostitle = $params->get('opensearch_title', JText::_('MOD_TANZIL_SEARCH_SEARCH') . ' ' . $app->getCfg('sitename'));
+	$doc->addHeadLink(
+			JUri::getInstance()->toString(array('scheme', 'host', 'port'))
+			. JRoute::_('&option=com_search&format=opensearch'), 'search', 'rel',
+			array(
+				'title' => htmlspecialchars($ostitle),
+				'type' => 'application/opensearchdescription+xml'
+			)
+		);
+}
+
+$maxlength = $lang->getUpperLimitSearchWord();
 
 $moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
+$set_Itemid      = (int) $params->get('set_itemid', 0);
+
+$mitemid = $set_Itemid > 0 ? $set_Itemid : $app->input->get('Itemid');
+
+$list = ModTanzilSearchHelper::getList($params);
 
 require JModuleHelper::getLayoutPath('mod_tanzil_search', $params->get('layout', 'default'));
--- Packages/mod_tanzil_search/tmpl/default.php
+++ Packages/mod_tanzil_search/tmpl/default.php
@@ -12,12 +12,24 @@ defined('_JEXEC') or die;
 
 <div class="tanzil-search<?php echo $moduleclass_sfx; ?>">
 	<ul class="nav nav-tabs">
-		<li class="active"><a href="#search-quran-tab" data-toggle="tab"><?php echo JText::_('MOD_TANZIL_SEARCH_QURAN'); ?></a></li>
+		<li class="active"><a href="#search-site-tab" data-toggle="tab"><?php echo JText::_('MOD_TANZIL_SEARCH_SITE'); ?></a></li>
+		<li><a href="#search-quran-tab" data-toggle="tab"><?php echo JText::_('MOD_TANZIL_SEARCH_QURAN'); ?></a></li>
 		<li><a href="#search-roots-tab" data-toggle="tab"><?php echo JText::_('MOD_TANZIL_SEARCH_ROOTS'); ?></a></li>
 		<li><a href="#search-goto-tab" data-toggle="tab"><?php echo JText::_('MOD_TANZIL_SEARCH_GOTO'); ?></a></li>
 	</ul>
 	<div class="tab-content">
-		<div id="search-quran-tab" class="tab-pane fade in active">
+		<div id="search-site-tab" class="tab-pane fade in active">
+			<form action="<?php echo JRoute::_('index.php');?>" method="post" class="form-search">
+				<div class="input-append row-fluid text-center">
+					<input name="searchword" id="mod-tanzil-search-searchword" type="text" placeholder="<?php echo JText::_('MOD_TANZIL_SEARCH_SEARCH_SITE'); ?>" class="span10 search-query" maxlength="<?php echo $maxlength; ?>" />
+					<button type="button" class="btn hasTooltip" onclick="this.form.submit();" title="<?php echo JText::_('MOD_TANZIL_SEARCH_SEARCH'); ?>"><i class="icon-search"></i></button>
+				</div>
+				<input type="hidden" name="task" value="search" />
+				<input type="hidden" name="option" value="com_search" />
+				<input type="hidden" name="Itemid" value="<?php echo $mitemid; ?>" />
+			</form>
+		</div>
+		<div id="search-quran-tab" class="tab-pane fade">
 			<form action="javascript:Search.go();" class="form-search">
 				<div class="input-append row-fluid text-center">
 					<input id="searchText" type="text" value="كتاب" class="span10 search-query" />
